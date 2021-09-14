36ebbb60a92880be472d46abea949d527268f961
Gaurav mishra <garvmishra9@gmail.com>
2021-09-14 12:54:21 +0530

    fix

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  plugins: [
+    require('daisyui'),
+    require('@tailwindcss/custom-forms'),
+  ],
+  theme: {
+    
+    extend: {
+      colors: {
+        // Colors you want to add go here
+        gray: colors.trueGray,
+        red: colors.red,
+        blue: colors.sky,
+        yellow: colors.amber, 
+        vblue: '#5950B7',
+        vgray: '#9EA3AE',
+        vgreen: '#D5FAFC',
+        vteal: '#2EC5CE',
+      },
+      fontFamily: {
+        roboto: ['Roboto'],
+        sanspro: ['Source Sans Pro'],
+        manrope: ['Manrope']
+
+      },
+      backgroundImage: {
+        'hero-lg': "url(https://i.ibb.co/XzyPwgq/Background.png)",
+        
+    },
+    }
+  }
+}
